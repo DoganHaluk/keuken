8714d16b35ac013872805baa953dc4b0260d66a5
DoganHaluk <ha_look@yahoo.com>
2021-08-09 02:39:21 +0200

    keuken application

--- src/test/java/be/vdab/keuken/repositories/JpaArtikelRepositoryTest.java
+++ src/test/java/be/vdab/keuken/repositories/JpaArtikelRepositoryTest.java
@@ -1,10 +1,14 @@
 package be.vdab.keuken.repositories;
 
+import be.vdab.keuken.domain.Artikel;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,6 +17,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Import(JpaArtikelRepository.class)
 class JpaArtikelRepositoryTest extends AbstractTransactionalJUnit4SpringContextTests {
     private final JpaArtikelRepository repository;
+    private static final String ARTIKELS = "artikels";
+    private Artikel artikel;
 
     public JpaArtikelRepositoryTest(JpaArtikelRepository repository) {
         this.repository = repository;
@@ -30,5 +36,16 @@ class JpaArtikelRepositoryTest extends AbstractTransactionalJUnit4SpringContextT
     @Test
     void findByOnbestaandeId() {
         assertThat(repository.findById(-1)).isNotPresent();
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        artikel=new Artikel("test", BigDecimal.ONE, BigDecimal.ONE);
+    }
+    @Test
+    void create(){
+        repository.create(artikel);
+        assertThat(artikel.getId()).isPositive();
+        assertThat(countRowsInTableWhere(ARTIKELS, "id="+artikel.getId())).isOne();
     }
 }
